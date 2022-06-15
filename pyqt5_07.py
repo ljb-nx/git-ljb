87081ac20e779d8dbc2ad292a2a8d196d019a069
ljb-nx <17752330606@163.com>
2022-06-15 22:43:15 +0800

    push pyqt5_07.py

---
+++ pyqt5_07.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#  coding:utf-8
+
+'''
+color = QColorDialog.getColor() # 颜色对话框（十六进制的值会保存在color变量中）
+if color.isValid(): # 判断color是否有效
+	self.text_edit.setTextColor(color)
+
+font, ok = QFontDialog.getFont() # 字体对话框
+if ok:
+	self.text_edit.setFont(font)
+'''
